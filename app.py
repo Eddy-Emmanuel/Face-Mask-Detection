c0a7bf5e518d23c28e251420baa4cc10c86bfb9f
EDIFON EMMANUEL JIMMY <98073355+Eddy-Emmanuel@users.noreply.github.com>
2024-02-28 22:35:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -21,8 +21,7 @@ def Run_Live_Test(source):
 
     if cam is not None:
         while loop:
-            frame = cam.read()
-            frame = imutils.resize(frame, width=1000, height=1000) # Step 4
+            frame = imutils.resize(cam.read(), width=1000, height=1000) # Step 4
             h, w, _ = frame.shape # Step 5
             blob = cv2.dnn.blobFromImage(frame, scalefactor=1.0, size=(300, 300), mean=(104, 177, 123)) # # Step 6
             face_detector.setInput(blob=blob) # Step 7
