190ef49db7185a8832f057eca29f77352d225762
EDIFON EMMANUEL JIMMY <98073355+Eddy-Emmanuel@users.noreply.github.com>
2024-02-28 21:59:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from keras.preprocessing.image import img_to_array
 from imutils.video import VideoStream, FileVideoStream
 
 classifier = load_model(r"mobilenetv2.h5") # Step 1
-face_detector = cv2.dnn.readNet(model=r"face_detector_model\deploy.prototxt",
-                                config=r"face_detector_model\res10_300x300_ssd_iter_140000.caffemodel") # Step 2
+face_detector = cv2.dnn.readNet(model="deploy.prototxt",
+                                config="res10_300x300_ssd_iter_140000.caffemodel") # Step 2
 
 def Run_Live_Test(source):
     loop = True
