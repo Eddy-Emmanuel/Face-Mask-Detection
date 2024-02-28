0f345bca8d74253857cdaf53ab0e19fe946d4e17
EDIFON EMMANUEL JIMMY <98073355+Eddy-Emmanuel@users.noreply.github.com>
2024-02-28 22:35:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from keras.applications import mobilenet_v2
 from keras.preprocessing.image import img_to_array
 from imutils.video import VideoStream, FileVideoStream
 
-classifier = load_model(r"mobilenetv2.h5") # Step 1
+classifier = load_model("mobilenetv2.h5") # Step 1
 face_detector = cv2.dnn.readNet(model="deploy.prototxt",
                                 config="res10_300x300_ssd_iter_140000.caffemodel") # Step 2
 
