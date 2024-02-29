0012d57731c65374edbbd527e7d777852b3877e9
EDIFON EMMANUEL JIMMY <98073355+Eddy-Emmanuel@users.noreply.github.com>
2024-02-29 13:37:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,78 +5,65 @@ import streamlit as st
 from keras.models import load_model
 from keras.applications import mobilenet_v2
 from keras.preprocessing.image import img_to_array
+from streamlit_webrtc import webrtc_streamer, WebRtcMode
+import av
 
 classifier = load_model("mobilenetv2.h5") # Step 1
 face_detector = cv2.dnn.readNet(model="deploy.prototxt",
                                 config="res10_300x300_ssd_iter_140000.caffemodel") # Step 2
 
-def Run_Live_Test(source):
-    loop = True
-    # cam = cv2.VideoCapture(0)
+
+def callback_(cam.VideoFrame)-> av.VideoFrame:
+    frame = cam.to_ndarray(format="bgr24")
   
-    def get_cap():
-        return cv2.VideoCapture(0)
-    cam = get_cap()
-    frameST = st.empty()
-    if cam.isOpened():
-        while loop:
-            _, frame = cam.read() # Step 4
-            h, w, _ = frame.shape # Step 5
-            blob = cv2.dnn.blobFromImage(frame, scalefactor=1.0, size=(300, 300), mean=(104, 177, 123)) # # Step 6
-            face_detector.setInput(blob=blob) # Step 7
-            detected_faces = face_detector.forward() # Step 8 -> Detect Faces
+    while True:
+        h, w, _ = frame.shape # Step 5
+        blob = cv2.dnn.blobFromImage(frame, scalefactor=1.0, size=(300, 300), mean=(104, 177, 123)) # # Step 6
+        face_detector.setInput(blob=blob) # Step 7
+        detected_faces = face_detector.forward() # Step 8 -> Detect Faces
 
-            preprocessed_face = []
-            face_loc = []
+        preprocessed_face = []
+        face_loc = []
 
-            for i in range(detected_faces.shape[2]): # Step 9
-                confidence = detected_faces[0, 0, i, 2] # Step 10
+        for i in range(detected_faces.shape[2]): # Step 9
+            confidence = detected_faces[0, 0, i, 2] # Step 10
 
-                if confidence > 0.5:
-                    x1, y1, x2, y2 = detected_faces[0, 0, i, 3:7] # Step 11
+            if confidence > 0.5:
+                x1, y1, x2, y2 = detected_faces[0, 0, i, 3:7] # Step 11
                     # Step 12
-                    startX, endX = int(x1*w), int(x2*w)
-                    startY, endY = int(y1*h), int(y2*h)
+                startX, endX = int(x1*w), int(x2*w)
+                startY, endY = int(y1*h), int(y2*h)
 
-                    startX, endX = max(0, startX), min(w-1, endX)
-                    startY, endY = max(0, startY), min(h-1, endY)
+                startX, endX = max(0, startX), min(w-1, endX)
+                startY, endY = max(0, startY), min(h-1, endY)
 
                     # Step 13
-                    face = frame[startY:endY, startX:endX]
-                    rgb_face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-                    prep_face = mobilenet_v2.preprocess_input(cv2.resize(rgb_face, (224, 224)))
-                    print(prep_face.shape)
-                    preprocessed_face.append(prep_face)
-                    face_loc.append([startX, startY, endX, endY])
+                face = frame[startY:endY, startX:endX]
+                rgb_face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+                prep_face = mobilenet_v2.preprocess_input(cv2.resize(rgb_face, (224, 224)))
+                preprocessed_face.append(prep_face)
+                face_loc.append([startX, startY, endX, endY])
 
-                else:
-                    continue
+            else:
+                continue
             
 
-            try:
-                if len(face) != 0:
-                    predictions = classifier.predict(np.array(preprocessed_face))
+        try:
+            if len(face) != 0:
+                predictions = classifier.predict(np.array(preprocessed_face))
 
-                for (PREDICTIONS, PRED_LOCATIONS) in zip(predictions, face_loc):
-                    with_facemask, without_facemask = PREDICTIONS
+            for (PREDICTIONS, PRED_LOCATIONS) in zip(predictions, face_loc):
+                with_facemask, without_facemask = PREDICTIONS
 
-                    color = (0, 0, 255) if without_facemask > with_facemask else (0, 255, 0)
-                    predicted_class = "Facemask on" if with_facemask > without_facemask else "Facemask off"
-                    MODEL_CONFIDENCE = with_facemask if with_facemask > without_facemask else without_facemask
+                color = (0, 0, 255) if without_facemask > with_facemask else (0, 255, 0)
+                predicted_class = "Facemask on" if with_facemask > without_facemask else "Facemask off"
+                MODEL_CONFIDENCE = with_facemask if with_facemask > without_facemask else without_facemask
 
-                    cv2.rectangle(frame, (PRED_LOCATIONS[0], PRED_LOCATIONS[1]), (PRED_LOCATIONS[2], PRED_LOCATIONS[3]), color, 2)
-                    cv2.putText(frame, f"{predicted_class}: {MODEL_CONFIDENCE:.2f}", (PRED_LOCATIONS[0], PRED_LOCATIONS[1]-40), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 2)
-                
-                # cv2.imshow("Camera", frame)
-                frameST.image(frame, channels="BGR")
+                cv2.rectangle(frame, (PRED_LOCATIONS[0], PRED_LOCATIONS[1]), (PRED_LOCATIONS[2], PRED_LOCATIONS[3]), color, 2)
+                cv2.putText(frame, f"{predicted_class}: {MODEL_CONFIDENCE:.2f}", (PRED_LOCATIONS[0], PRED_LOCATIONS[1]-40), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 2)
+          
 
-                if cv2.waitKey(1) == ord("q"):
-                    loop = False
-            except:
-                continue
-
-        cam.release()
-        cv2.destroyAllWindows()
+    return av.VideoFrame.from_ndarray(frame, format="brg24")
 
 
 st.markdown("<h1><center>Face Mask Detection 😎<center/><h1/>", unsafe_allow_html=True)
@@ -85,7 +72,15 @@ options = st.sidebar.selectbox(label="Select Operation", options=["None", "Live 
 
 if options != "None":
     if options == "Live Test":
-        Run_Live_Test(source=0)
+        webrtc_streamer(
+                        key="object-detection",
+                        mode=WebRtcMode.SENDRECV,
+                         video_frame_callback=callback_,
+                         media_stream_constraints={"video": True, "audio": False},
+                         async_processing=True,
+                                )
+
+  
 
     if options == "HeadShot Test":
         picture = st.file_uploader("Upload Picture", ["png ", "jpg"], accept_multiple_files=False)
