d11e5287d4738891f10ef28f20028718b9b35db1
EDIFON EMMANUEL JIMMY <98073355+Eddy-Emmanuel@users.noreply.github.com>
2024-02-29 16:39:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,167 +5,177 @@ import streamlit as st
 from keras.models import load_model
 from keras.applications import mobilenet_v2
 from keras.preprocessing.image import img_to_array
-from streamlit_webrtc import webrtc_streamer,  VideoTransformerBase
+from streamlit_webrtc import webrtc_streamer, VideoProcessorBase, WebRtcMode
+import av
 
-# classifier = load_model("mobilenetv2.h5") # Step 1
-# face_detector = cv2.dnn.readNet(model="deploy.prototxt",
-#                                 config="res10_300x300_ssd_iter_140000.caffemodel") # Step 2
+classifier = load_model("mobilenetv2.h5") # Step 1
+face_detector = cv2.dnn.readNet(model="deploy.prototxt",
+                                config="res10_300x300_ssd_iter_140000.caffemodel") # Step 2
 
-# # RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
-
-# class VideoDisplay(VideoTransformerBase):
-#       def CALLBACK(self, cam):
-#           frame = cam.to_ndarray(format="bgr24")
+class VideoDisplay(VideoTransformerBase):
+      def CALLBACK(self, cam: av.VideoFrame) -> av.VideoFrame:
+          frame = cam.to_ndarray(format="bgr24")
       
-#           h, w, _ = frame.shape
-#           blob = cv2.dnn.blobFromImage(frame, scalefactor=1.0, size=(300, 300), mean=(104, 177, 123))
-#           face_detector.setInput(blob=blob)
-#           detected_faces = face_detector.forward()
+          h, w, _ = frame.shape
+          blob = cv2.dnn.blobFromImage(frame, scalefactor=1.0, size=(300, 300), mean=(104, 177, 123))
+          face_detector.setInput(blob=blob)
+          detected_faces = face_detector.forward()
       
-#           for i in range(detected_faces.shape[2]):
-#               confidence = detected_faces[0, 0, i, 2]
+          for i in range(detected_faces.shape[2]):
+              confidence = detected_faces[0, 0, i, 2]
       
-#               if confidence > 0.5:
-#                   x1, y1, x2, y2 = detected_faces[0, 0, i, 3:7]
-#                   startX, endX = max(0, int(x1 * w)), min(w - 1, int(x2 * w))
-#                   startY, endY = max(0, int(y1 * h)), min(h - 1, int(y2 * h))
+              if confidence > 0.5:
+                  x1, y1, x2, y2 = detected_faces[0, 0, i, 3:7]
+                  startX, endX = max(0, int(x1 * w)), min(w - 1, int(x2 * w))
+                  startY, endY = max(0, int(y1 * h)), min(h - 1, int(y2 * h))
       
-#                   face = frame[startY:endY, startX:endX]
-#                   rgb_face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-#                   prep_face = mobilenet_v2.preprocess_input(cv2.resize(rgb_face, (224, 224)))
+                  face = frame[startY:endY, startX:endX]
+                  rgb_face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+                  prep_face = mobilenet_v2.preprocess_input(cv2.resize(rgb_face, (224, 224)))
                       
-#                   with_facemask, without_facemask = classifier.predict(np.expand_dims(prep_face, axis=0))[0]
-#                   color = (0, 0, 255) if without_facemask > with_facemask else (0, 255, 0)
-#                   predicted_class = "Facemask on" if with_facemask > without_facemask else "Facemask off"
+                  with_facemask, without_facemask = classifier.predict(np.expand_dims(prep_face, axis=0))[0]
+                  color = (0, 0, 255) if without_facemask > with_facemask else (0, 255, 0)
+                  predicted_class = "Facemask on" if with_facemask > without_facemask else "Facemask off"
       
-#                   cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
-#                   cv2.putText(frame, f"{predicted_class}: {max(with_facemask, without_facemask):.2f}", 
-#                               (startX, startY - 40), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 2)
+                  cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+                  cv2.putText(frame, f"{predicted_class}: {max(with_facemask, without_facemask):.2f}", 
+                              (startX, startY - 40), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 2)
       
-#           return frame
+          return return av.VideoFrame.from_ndarray(frame, format="bgr24")
 
 
 
-# st.markdown("<h1><center>Face Mask Detection 😎<center/><h1/>", unsafe_allow_html=True)
+st.markdown("<h1><center>Face Mask Detection 😎<center/><h1/>", unsafe_allow_html=True)
 
-# options = st.sidebar.selectbox(label="Select Operation", options=["None", "Live Test", "HeadShot Test", "Full Body Pics Test"])
+options = st.sidebar.selectbox(label="Select Operation", options=["None", "Live Test", "HeadShot Test", "Full Body Pics Test"])
 
-# def main():
-#   if options != "None":
-#       if options == "Live Test":
-#           webrtc_streamer(key="sample", video_processor_factory=VideoDisplay)
+def main():
+  if options != "None":
+    if options == "Live Test":
+        stream = webrtc_streamer(
+                                    key="barcode-detection",
+                                    mode=WebRtcMode.SENDRECV,
+                                    video_processor_factory=VideoTransformerBase,
+                                    media_stream_constraints={"video": True, "audio": False},
+                                    async_processing=True,
+                                    )
+
+    while True:
+        if stream.video_processor.barcode_val != False:
+            barcode = stream.video_processor.barcode_val
+            del stream
     
   
-#       if options == "HeadShot Test":
-#           picture = st.file_uploader("Upload Picture", ["png ", "jpg"], accept_multiple_files=False)
-#           if picture is not None:
-#               image = Image.open(picture)
-#               converted_image_to_array = cv2.resize(cv2.cvtColor(img_to_array(image), cv2.COLOR_RGBA2RGB), (224, 224))
-#               preprocessed_image = mobilenet_v2.preprocess_input(converted_image_to_array)
-#               predictions = classifier.predict(np.expand_dims(preprocessed_image, axis=0))
-#               st.markdown(f"Prediction: {'FaceMask On' if predictions[0][0] > predictions[0][1] else 'FaceMask Off'}")
-#               st.image(image=image, caption="Model Ouput")
+      if options == "HeadShot Test":
+          picture = st.file_uploader("Upload Picture", ["png ", "jpg"], accept_multiple_files=False)
+          if picture is not None:
+              image = Image.open(picture)
+              converted_image_to_array = cv2.resize(cv2.cvtColor(img_to_array(image), cv2.COLOR_RGBA2RGB), (224, 224))
+              preprocessed_image = mobilenet_v2.preprocess_input(converted_image_to_array)
+              predictions = classifier.predict(np.expand_dims(preprocessed_image, axis=0))
+              st.markdown(f"Prediction: {'FaceMask On' if predictions[0][0] > predictions[0][1] else 'FaceMask Off'}")
+              st.image(image=image, caption="Model Ouput")
       
-#       if options == "Full Body Pics Test":
-#           picture = st.file_uploader("Upload Picture", ["png ", "jpg"], accept_multiple_files=False)
-#           if picture is not None:
-#               image = cv2.cvtColor(cv2.imdecode(np.frombuffer(picture.read(), np.uint8), 1), cv2.COLOR_BGR2RGB)
-#               h, w, _ = image.shape
-#               Blob = cv2.dnn.blobFromImage(cv2.cvtColor(image, cv2.COLOR_RGBA2RGB), scalefactor=1.3, size=(300, 300), mean=(104, 177, 123))
-#               face_detector.setInput(blob=Blob)
-#               detected_faces = face_detector.forward()
+      if options == "Full Body Pics Test":
+          picture = st.file_uploader("Upload Picture", ["png ", "jpg"], accept_multiple_files=False)
+          if picture is not None:
+              image = cv2.cvtColor(cv2.imdecode(np.frombuffer(picture.read(), np.uint8), 1), cv2.COLOR_BGR2RGB)
+              h, w, _ = image.shape
+              Blob = cv2.dnn.blobFromImage(cv2.cvtColor(image, cv2.COLOR_RGBA2RGB), scalefactor=1.3, size=(300, 300), mean=(104, 177, 123))
+              face_detector.setInput(blob=Blob)
+              detected_faces = face_detector.forward()
               
               
-#               for i in range(detected_faces.shape[2]):
-#                   confidence = detected_faces[0, 0, i, 2]
-#                   if confidence > 0.5:
-#                       x1, y1, x2, y2 = detected_faces[0, 0, i, 3:7]
-#                       startx, endx = int(x1*w), int(x2*w)
-#                       starty, endy = int(y1*h), int(y2*h)
+              for i in range(detected_faces.shape[2]):
+                  confidence = detected_faces[0, 0, i, 2]
+                  if confidence > 0.5:
+                      x1, y1, x2, y2 = detected_faces[0, 0, i, 3:7]
+                      startx, endx = int(x1*w), int(x2*w)
+                      starty, endy = int(y1*h), int(y2*h)
   
-#                       startx, endx = max(0, startx), min(w-1, endx)
-#                       starty, endy = max(0, starty), min(h-1, endy)
+                      startx, endx = max(0, startx), min(w-1, endx)
+                      starty, endy = max(0, starty), min(h-1, endy)
   
-#                       frame = image[starty:endy, startx:endx] 
-#                       rgb_face = cv2.cvtColor(frame, cv2.COLOR_RGBA2RGB)
-#                       prep_face = mobilenet_v2.preprocess_input(cv2.resize(rgb_face, (224, 224)))
+                      frame = image[starty:endy, startx:endx] 
+                      rgb_face = cv2.cvtColor(frame, cv2.COLOR_RGBA2RGB)
+                      prep_face = mobilenet_v2.preprocess_input(cv2.resize(rgb_face, (224, 224)))
                           
-#                       with_facemask, without_facemask = classifier.predict(np.expand_dims(prep_face, axis=0))[0]
-#                       color = (0, 0, 255) if without_facemask > with_facemask else (0, 255, 0)
-#                       predicted_class = "Facemask on" if with_facemask > without_facemask else "Facemask off"
+                      with_facemask, without_facemask = classifier.predict(np.expand_dims(prep_face, axis=0))[0]
+                      color = (0, 0, 255) if without_facemask > with_facemask else (0, 255, 0)
+                      predicted_class = "Facemask on" if with_facemask > without_facemask else "Facemask off"
   
-#                       cv2.rectangle(image, (startx, starty), (endx, endy), color, 2)
-#                       cv2.putText(image, f"{predicted_class}", (startx, starty-40), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 2)
+                      cv2.rectangle(image, (startx, starty), (endx, endy), color, 2)
+                      cv2.putText(image, f"{predicted_class}", (startx, starty-40), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 2)
               
-#               st.image(image=image, caption="Output")
+              st.image(image=image, caption="Output")
 
-import av
-import cv2
-import time
-import streamlit as st 
-from pyzbar.pyzbar import decode
-from streamlit_webrtc import (webrtc_streamer, VideoProcessorBase,WebRtcMode)
+# import av
+# import cv2
+# import time
+# import streamlit as st 
+# from pyzbar.pyzbar import decode
+# from streamlit_webrtc import (webrtc_streamer, VideoProcessorBase,WebRtcMode)
 
-st.set_page_config(layout="wide")
+# st.set_page_config(layout="wide")
 
-def live_detection(play_state):
+# def live_detection(play_state):
 
-   c1, c2 = st.columns(2)
-   class BarcodeProcessor(VideoProcessorBase):
+#    c1, c2 = st.columns(2)
+#    class BarcodeProcessor(VideoProcessorBase):
 
-      def __init__(self) -> None:
-         self.barcode_val = False
+#       def __init__(self) -> None:
+#          self.barcode_val = False
       
-      def BarcodeReader(self, image):
-         detectedBarcodes = decode(image)
-         if not detectedBarcodes:
-            print("\n No barcode! \n")
-            return image, False
+#       def BarcodeReader(self, image):
+#          detectedBarcodes = decode(image)
+#          if not detectedBarcodes:
+#             print("\n No barcode! \n")
+#             return image, False
 
-         else:
-            for barcode in detectedBarcodes: 
-               (x, y, w, h) = barcode.rect
-               cv2.rectangle(image, (x-10, y-10),
-                              (x + w+10, y + h+10),
-                              (0, 255, 0), 2)
+#          else:
+#             for barcode in detectedBarcodes: 
+#                (x, y, w, h) = barcode.rect
+#                cv2.rectangle(image, (x-10, y-10),
+#                               (x + w+10, y + h+10),
+#                               (0, 255, 0), 2)
 
-            if detectedBarcodes[0] != "":
-               return image, detectedBarcodes[0]
+#             if detectedBarcodes[0] != "":
+#                return image, detectedBarcodes[0]
 
 
-      def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
-         image = frame.to_ndarray(format="bgr24")
+#       def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
+#          image = frame.to_ndarray(format="bgr24")
 
-         annotated_image, result = self.BarcodeReader(image)
+#          annotated_image, result = self.BarcodeReader(image)
 
-         if result == False:
-            return av.VideoFrame.from_ndarray(annotated_image, format="bgr24")
-         else:
-            self.barcode_val = result[0]
-            return av.VideoFrame.from_ndarray(annotated_image, format="bgr24")
+#          if result == False:
+#             return av.VideoFrame.from_ndarray(annotated_image, format="bgr24")
+#          else:
+#             self.barcode_val = result[0]
+#             return av.VideoFrame.from_ndarray(annotated_image, format="bgr24")
 
-   stream = webrtc_streamer(
-         key="barcode-detection",
-         mode=WebRtcMode.SENDRECV,
-         desired_playing_state=play_state,
-         video_processor_factory=BarcodeProcessor,
-         media_stream_constraints={"video": True, "audio": False},
-         async_processing=True,
-      )
+#    stream = webrtc_streamer(
+#          key="barcode-detection",
+#          mode=WebRtcMode.SENDRECV,
+#          desired_playing_state=play_state,
+#          video_processor_factory=BarcodeProcessor,
+#          media_stream_constraints={"video": True, "audio": False},
+#          async_processing=True,
+#       )
 
-   while True:
-      if stream.video_processor.barcode_val != False:
-         barcode = stream.video_processor.barcode_val
-         print("FOUND")
-         c1.subheader(barcode)
-         del stream
+#    while True:
+#       if stream.video_processor.barcode_val != False:
+#          barcode = stream.video_processor.barcode_val
+#          print("FOUND")
+#          c1.subheader(barcode)
+#          del stream
 
-play_state = True
+# play_state = True
 
-possible_barcode = live_detection(play_state)
+# possible_barcode = live_detection(play_state)
 
-# if __name__ == "__main__":
-#   main()
+if __name__ == "__main__":
+  main()
 
 
 
